13d136239fa6b2bf0e483f663a78fb6a25156771
Akash82945 <akashkumar9576386319@gmail.com>
2026-07-07 22:58:30 +0530

    Updated code.

--- 01-sales-analysis/src/04.data_visualization.py
+++ 01-sales-analysis/src/04.data_visualization.py
@@ -57,44 +57,107 @@ from pyparsing import Char
 
 
 df = pd.read_csv('data/Clean_sales.csv')
-print(df.cloumn)
+print(df.columns)
 
 
+# Category Revenue
 category_revenue = df.groupby('Category')['Final Amount'].sum().reset_index()
+
+# Payment Mode
 payment_mode = df['Payment Mode'].value_counts()
+
+# Monthly Trend
 df['OrderDate'] = pd.to_datetime(df['OrderDate'])
 df = df.sort_values('OrderDate')
-monthly_date = df.groupby('Months')['Final Amount'].sum().reindex(['January', 'Februaru', 'March'])
+monthly_date = df.groupby(df['OrderDate'].dt.to_period('M'))['Final Amount'].sum().reset_index()
+monthly_date['OrderDate'] = monthly_date['OrderDate'].astype(str)
+
+# Customer Revenue
 customer_revn = df.groupby('CustomerID')['Final Amount'].sum().reset_index()
-category_discount = df.groupby('Catogary')['Discount'].sum().reset_index()
+
+# Category wise Discount
+category_discount = df.groupby('Category')['Discount'].sum().reset_index()
 
 
-fig, axes = plt.subplots(nrows=2 , ncols=2, figsize=(10,8))
 
 
-# 1. Bar chart
-# Category Revenue
-axes[0,0].bar(category_revenue, color='red', label='Category Revenue')
+# 1. Bar Chart: Category Revenue
+category = df['Category']
+revenue = df['Final Amount']
+plt.bar(category, revenue, label='Category', color='red', edgecolor='black')
+plt.title('Category wise Analysis.')
+plt.xlabel('Product Category.')
+plt.ylabel('Revenue (INR)')
+plt.legend()
+plt.grid(True, axis='y', linestyle='--')
+plt.savefig('visuals/01 Revenue by Category (BAR)')
+plt.show()
+
+
+# 2. Monthly Revenue Trend
+plt.plot(monthly_date['OrderDate'], monthly_date['Final Amount'], label='Months', color='orange')
+plt.title('Monthly Revenue Trend. (LINE)')
+plt.xlabel('Months Name')
+plt.ylabel('Revenue')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig('visuals/02 Monthly Revenue Trend (Line).')
+plt.show()
+
+
+plt.pie(df['Category'], autopct='%1.1f%%', labels=df['Category'])
+plt.title('Revenue Share by Category.(PIE)')
+plt.legend()
+plt.savefig('visuals/03 Revenue sahre by categpry.')
+plt.show()
+
+
+
+
+fig, axes = plt.subplots(nrows=2 , ncols=3, figsize=(10,8))
+
+
+# 1. Bar chart: Category Revenue
+axes[0,0].bar(category_revenue['Category'], category_revenue['Final Amount'], color='red', label='Category Revenue')
 axes[0,0].set_title('Category Wise Revenue')
-axes[0,0].set_xlabel('Category')
+axes[0,0].set_xlabel('Category', fontsize=8)
 axes[0,0].set_ylabel('Revenue')
-axes[0,0]
+axes[0,0].tick_params(axis='x', rotation=30  )
 
-# 2. Pie Chart
-# Paymrnt Mode
-
-
+# 2. Pie Chart: Payment Mode
+# axes[0,1].pie(payment_mode, autopct='%1.1f%%', startangle=90, labels=payment_mode.index)
+# axes[0,1].set_title('Payment Mode')
+category_sales = df.groupby('Category')['Amount_INR'].sum().reset_index()
+axes[0,1].pie(category_sales['Amount_INR'], labels=category_sales['Category'], autopct= '%1.1f%%', startangle=90)
+axes[0,1].set_title('Category Wise Revenue')
 
 # 3. Line Char
 # Monthly Sales Trend
-
+axes[0,2].plot(monthly_date['OrderDate'], monthly_date['Final Amount'], marker='x', markersize=4, color='Blue' )
+axes[0,2].set_title('Monthly Sales Trend')
+axes[0,2].set_xlabel('Months')
+axes[0,2].set_ylabel('Revenue')
+axes[0,2].tick_params(axis='x', rotation=30)
 
 
 # 4. Histogram
 # Customer Revenue
-
-
+axes[1,0].hist(df['CustomerID'].astype(str), bins=10, edgecolor='black')
+axes[1,0].set_title('Customer Revenue')
+axes[1,0].set_ylabel('Customer Count')
+axes[1,0].set_xlabel('Customer ID')
+axes[1,0].tick_params(axis='x', rotation=30, labelsize=8)
 
 
 # 5. Scattor plot
-# Category wise discout
+# Category wise discount
+axes[1,1].scatter(category_discount['Category'], category_discount['Discount'], color='orange')
+axes[1,1].set_title('Category Wise Discount.')
+axes[1,1].set_ylabel('Discount INR')
+axes[1,1].set_xlabel('Category')
+axes[1,1].tick_params(axis='x', rotation=30)
+
+
+plt.tight_layout()
+plt.show()
